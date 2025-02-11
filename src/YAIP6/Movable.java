db75028c33ab91e59c09f92f36b7c2cae73a651a
Demba <demba.kamara@etu.univ-amu.fr>
2025-02-11 17:14:49 +0100

    ajout de l'exo 6.3

---
+++ src/YAIP6/Movable.java
@@ -0,0 +1,9 @@
+package YAIP6;
+
+public interface Movable {
+	public void moveUp();
+	public void moveDown();
+	public void moveLeft();
+	public void moveRight();
+}
+
---
+++ src/YAIP6/MovablePoint.java
@@ -0,0 +1,36 @@
+package YAIP6;
+
+public class MovablePoint {
+	private int x;
+	private int y;
+	private int xSpeed;
+	private int ySpeed;
+	
+	public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+		this.x = x;
+		this.y = y;
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
+	}
+
+	@Override
+	public String toString() {
+		return "MovablePoint [x=" + x + ", y=" + y + ", xSpeed=" + xSpeed + ", ySpeed=" + ySpeed + "]";
+	}
+	
+	public void moveUp() {
+		this.y = this.y - ySpeed;
+	}
+	
+	public void moveDown() {
+		this.y = this.y + ySpeed;
+	}
+	
+	public void moveLeft() {
+		this.x = this.x - xSpeed;
+	}
+	
+	public void moveRight() {
+		this.x = this.x + xSpeed;
+	}
+}
